1d7f0e8c8675848f55b29c4fabd2ac8c62d50e30
Hannele Ruiz <justlemoncl@gmail.com>
2023-01-19 19:17:27 -0300

    Added missing localizations

--- leek_tags/tags.py
+++ leek_tags/tags.py
@@ -1,6 +1,6 @@
 from aiomysql import Cursor
 from discord import Cog, ApplicationContext, slash_command, Option, AutocompleteContext
-from leek import DatabaseRequiredError, LeekBot, localize
+from leek import DatabaseRequiredError, LeekBot, localize, get_localizations, get_default
 from pymysql import IntegrityError
 
 CREATE = "CREATE TABLE IF NOT EXISTS tags_%s (id INT NOT NULL auto_increment, name TEXT NOT NULL UNIQUE, " \
@@ -38,11 +38,10 @@ class Tags(Cog):
         if not self.bot.is_pool_available:
             raise DatabaseRequiredError(self)
 
-    @slash_command()
+    @slash_command(name_localizations=get_localizations("TAGS_COMMAND_TAG_NAME"),
+                   description=get_default("TAGS_COMMAND_TAG_DESC"),
+                   description_localization=get_localizations("TAGS_COMMAND_TAG_DESC"))
     async def tag(self, ctx: ApplicationContext, name: Option(str, "The tag name", autocomplete=get_tag_names)):
-        """
-        Views a specific tag.
-        """
         async with self.bot.connection as connection:
             cursor: Cursor = await connection.cursor()
             await cursor.execute(CREATE, [ctx.interaction.guild.id])
@@ -56,12 +55,11 @@ class Tags(Cog):
         else:
             await ctx.respond(tag[0])
 
-    @slash_command()
+    @slash_command(name_localizations=get_localizations("TAGS_COMMAND_CREATETAG_NAME"),
+                   description=get_default("TAGS_COMMAND_CREATETAG_DESC"),
+                   description_localization=get_localizations("TAGS_COMMAND_CREATETAG_DESC"))
     async def createtag(self, ctx: ApplicationContext, name: Option(str, "The tag name"),
                         content: Option(str, "The text content of the tag")):
-        """
-        Creates a new tag.
-        """
         try:
             async with self.bot.connection as connection:
                 cursor: Cursor = await connection.cursor()
@@ -73,11 +71,10 @@ class Tags(Cog):
         except IntegrityError:
             await ctx.respond(localize("TAGS_ADD_DUPE", ctx.locale, name), ephemeral=True)
 
-    @slash_command()
+    @slash_command(name_localizations=get_localizations("TAGS_COMMAND_DELETETAG_NAME"),
+                   description=get_default("TAGS_COMMAND_DELETETAG_DESC"),
+                   description_localization=get_localizations("TAGS_COMMAND_DELETETAG_DESC"))
     async def deletetag(self, ctx: ApplicationContext, name: Option(str, "The tag name", autocomplete=get_tag_names)):
-        """
-        Deletes a specific tag.
-        """
         async with self.bot.connection as connection:
             cursor: Cursor = await connection.cursor()
             await cursor.execute(CREATE, [ctx.interaction.guild.id])
